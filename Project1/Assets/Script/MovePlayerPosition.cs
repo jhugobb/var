5d2f271beabb978fde3968cbdaa60398645b91a8
Hugo Brandão <hugo.b.brandao@gmail.com>
2018-10-02 18:55:32 +0200

    Add project with teleport

---
+++ Project1/Assets/Script/MovePlayerPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayerPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void Teleport    ()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {
+            Vector3 pos = new Vector3();
+            pos.Set(hit.point.x, transform.position.y, hit.point.z);
+            transform.position = pos;
+
+            // Do something with the object that was hit by the raycast.
+        }
+
+    }
+}
