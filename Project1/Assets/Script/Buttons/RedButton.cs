6f807678c6ed864d1c9a4939c162281c5d2993a5
Hugo Brandão <hugo.b.brandao@gmail.com>
2018-10-06 21:52:31 +0200

    Add labyrinth with one button

---
+++ Project1/Assets/Script/Buttons/RedButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedButton : MonoBehaviour {
+
+
+    private bool isActivated;
+    private float spinForce = 20;
+
+    Door door;
+    // Use this for initialization
+    void Start () {
+        isActivated = false;
+        Renderer myRenderer = GetComponent<Renderer>();
+        myRenderer.material.color = Color.red;
+
+        door = GameObject.FindGameObjectWithTag("redDoor1").GetComponent(typeof(Door)) as Door;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (isActivated)
+        {
+            transform.Rotate(0, spinForce * Time.deltaTime, 0);
+        }
+	}
+
+    public void Toggle()
+    {
+        if (!isActivated)
+        {
+            door.OpenDoor();
+            isActivated = true;
+
+        } else
+        {
+            door.CloseDoor();
+            isActivated = false;
+        }
+    }
+}
--- Project1/Assets/Script/MovePlayerPosition.cs
+++ Project1/Assets/Script/MovePlayerPosition.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class MovePlayerPosition : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private Vector3 newPosition;
+    private Vector3 direction;
+    private double distance;
+
+    public int playerSpeed;
+
+    public float CLOSE_ENOUGH = 0.06f;
+
+
+    // Use this for initialization
+    void Start () {
+        playerSpeed = 4;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+        if (distance > CLOSE_ENOUGH)
+        {
+            transform.position = transform.position + direction * playerSpeed * Time.deltaTime;
+            direction = newPosition - transform.position;
+            direction.Normalize();
+            distance = Vector3.Distance(transform.position, newPosition);
+        }
     }
 
     public void Teleport    ()
@@ -20,12 +35,18 @@ public class MovePlayerPosition : MonoBehaviour {
 
         if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            Vector3 pos = new Vector3();
-            pos.Set(hit.point.x, transform.position.y, hit.point.z);
-            transform.position = pos;
+            newPosition.Set(hit.point.x, transform.position.y, hit.point.z);
+            
+            direction.Normalize();
+            distance = Vector3.Distance(transform.position, newPosition);
 
             // Do something with the object that was hit by the raycast.
         }
 
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        distance = 0;
     }
 }
