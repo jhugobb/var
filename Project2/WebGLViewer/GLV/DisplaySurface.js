5bcf8a6a3ed565f6b6683a4a8c721dac44d59f17
jhugobb <hugo.b.brandao@gmail.com>
2018-11-12 19:42:44 +0100

    Add webgl

--- Project2/WebGLViewer/GLV/DisplaySurface.js
+++ Project2/WebGLViewer/GLV/DisplaySurface.js
@@ -1,32 +1,52 @@
-// Class DisplaySurface: walls of a projection-based system
-
-DisplaySurface = function(orig, uvector, vvector){ 
-    this.origin = orig; // Vec3 - Origin of the display
-    this.u = uvector;   // Vec3 - Horizontal vector
-    this.v = vvector;   // Vec3 - Vertical vector
-};
-
-// Functions
-
-DisplaySurface.prototype.viewingMatrix = function(eye){
-	mat = new Mat4();
-    mat.loadIdentity();
-	
-	// TBC: this example does NOT create the correct viewing matrix
-	var eye = new Vec3(0.0, 50.0, 100.0);
-	var vrp = new Vec3(0.0, 0.0, 0.0);
-	var up = new Vec3(0.0, 1.0, 0.0);
-	mat.lookAt(eye, vrp, up);
-	
-	return mat;
-};
-
-DisplaySurface.prototype.projectionMatrix = function(eye, znear, zfar){
-	mat = new Mat4();
-    mat.loadIdentity();
-    
-	// TBC: this example does NOT create the correct projection matrix
-	mat.frustum(-5, 5, -5, 5, 10, 1000);
-	
-	return mat;
-};
+// Class DisplaySurface: walls of a projection-based system 
+ 
+DisplaySurface = function(orig, uvector, vvector){  
+    this.origin = orig; // Vec3 - Origin of the display 
+    this.u = uvector;   // Vec3 - Horizontal vector 
+    this.v = vvector;   // Vec3 - Vertical vector 
+}; 
+ 
+// Functions 
+ 
+DisplaySurface.prototype.viewingMatrix = function(eye){ 
+    var mat = new Mat4(); 
+    mat.loadIdentity(); 
+    a = new Vec3(this.u.x, this.u.y,this.u.z); 
+    a = a.normalize(); 
+    b = new Vec3(this.v.x, this.v.y,this.v.z); 
+    b = b.normalize(); 
+    c = Vec3.cross(a,b); 
+     
+    focus = Vec3.subtract(eye, c); 
+    normal = new Vec3(0, eye.y,0); 
+    mat.lookAt(eye, focus,normal); 
+    return mat; 
+}; 
+ 
+DisplaySurface.prototype.projectionMatrix = function(eye, znear, zfar){ 
+    var mat = new Mat4(); 
+    mat.loadIdentity(); 
+    var left, right, top, bottom; 
+	var horizontalDistance=this.u.norm()/2;
+    u = new Vec3(this.u.x, this.u.y,this.u.z); 
+    u = u.normalize(); 
+	var vertitalDistance=this.v.norm()/2;
+    v = new Vec3(this.v.x, this.v.y,this.v.z); 
+    v = v.normalize(); 
+    n = Vec3.cross(u,v); 
+     
+    // Dot products needed 
+    var uDOTe = Vec3.dot(u, eye); 
+    var vDOTe = Vec3.dot(v, eye); 
+    var nDOTe = Vec3.dot(n, eye); 
+ 
+    // Calculating the distances of the display-dependent frustum 
+    left=-znear*(horizontalDistance+uDOTe)/(horizontalDistance+nDOTe); 
+    right=znear*(horizontalDistance-uDOTe)/(horizontalDistance+nDOTe); 
+    top=-znear*(vertitalDistance+vDOTe)/(vertitalDistance+ nDOTe); 
+    bottom=znear*(vertitalDistance-vDOTe)/(vertitalDistance+nDOTe); 
+             
+    mat.frustum(left, right, top, bottom, znear, zfar); 
+ 
+    return mat; 
+}; 
